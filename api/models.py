c1cc91f7b73abf96f98094c64f10e5a6bc6b442c
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-16 18:57:22 +0500

    register model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Smartphones(models.Model):
+    price = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    ram = models.CharField(max_length=255)
+    memory = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
